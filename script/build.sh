efcf204b4fcc0df8ac1bc2f62b4a49fc021c4863
Lwh2008 <lwh20082021@outlook.com>
2025-10-02 21:46:50 +0800

    1

---
+++ script/build.sh
@@ -0,0 +1,5 @@
+# compile Linux aach64
+dart compile exe --target-os=linux --target-arch=arm64 lib/main.dart -o build/linux_arm64/file_syncer_cli
+
+# compile armV7
+#dart compile exe --target-os=linux --target-arch=arm lib/main.dart -o build/linux_arm/file_syncer_cli
